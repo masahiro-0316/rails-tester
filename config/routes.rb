093ef6ee993a05a82011094b96d08f4a8cd7e307
masahiro <masahiro@sefira.arcana>
2023-04-08 16:14:19 +0900

    学習成果　#3

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  namespace :admins, path: "/-/root" do
+    root "top#index"
+    devise_for :admins, :controller => {
+    }
+  end
+  devise_for :users
   # Defines the root path route ("/")
   # root "articles#index"
 end
