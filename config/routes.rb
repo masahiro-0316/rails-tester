88b62d3ab0e62865d3f595a6c3636362a7e97e99
masahiro <masahiro@sefira.arcana>
2023-04-09 19:55:34 +0900

    ルーティング確認 #3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
- # namespace :admin do
- #   get 'top/index'
- # end
+  devise_for :admin_users, controllers: {
+    registrations: 'admin/registrations',
+    sessions: 'admin/sessions'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -9,12 +10,5 @@ Rails.application.routes.draw do
 
 namespace :admin do
   root to: "top#index"
-  devise_for :admin_users, controllers: {
-    registrations: 'admin/registrations',
-    sessions: 'admin/sessions'
-  }
-#  devise_scope :admin_users do
-#    get 'sigin_in' to: 'dmin#index'
-#  end
   end
 end
